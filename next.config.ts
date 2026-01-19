ba6e794b817e7638336da49976d998c11e482a0f
Kavindu Wijesekara <tech.kavinduwijesekara@gmail.com>
2026-01-19 08:57:24 +0530

    third commit

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,7 @@ const nextConfig: NextConfig = {
     // TypeScript වැරදි තිබුනත් Deploy වෙනවා
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // ESLint වැරදි තිබුනත් Deploy වෙනවා
-    ignoreDuringBuilds: true,
-  },
+  
 };
 
 export default nextConfig;
